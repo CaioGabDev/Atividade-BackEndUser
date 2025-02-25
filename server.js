791ae4006d253c8d9fda60f6638e0ae736121e45
Caio Lacerda <lacerdacaiogabriel@gmail.com>
2025-02-25 10:34:56 -0300

    feat: create models, controllers and routes post

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const usersRoutes = require("./src/routes/usersRoutes");
+const userRoutes = require("./src/routes/userRoutes");
+const postRoutes = require("./src/routes/postRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -10,7 +11,8 @@ const PORT = process.env.PORT || 4000;
 app.use(cors()); 
 app.use(express.json()); 
 
-app.use("/api", usersRoutes);
+app.use("/api", userRoutes);
+app.use("/api", postRoutes);
 
 app.get("/", (req, res) => {
     res.send("ROTA TA FUNCIONANDO!!");
---
+++ src/controllers/postController.js
@@ -0,0 +1,50 @@
+const Post = require("../models/Post");
+const PostList = require("../models/PostList");
+const lista = new PostList();
+
+
+lista.addPost(new Post("", 1000, 400, "Foto do meu dog", "https://www.dog.com"));
+lista.addPost(new Post("", 300, 20, "lolzinho", "https://www.lol.com"));
+
+const router = {
+  getAllPosts: (req, res) => {
+    res.json(lista.getAllPosts());
+  },
+
+  getPostById: (req, res) => {
+    try {
+      res.json(lista.getPostById(req.params.id));
+    } catch (error) {
+      res.status(404).json({ message: "Esse post não existe em nosa rede", error });
+    }
+  },
+
+  addPost: (req, res) => {
+    try {
+      const { idUser, like, comment, description, image } = req.body;
+      if (!idUser, !like, !comment, !description, !image) {
+        throw new Error("É necessário preencher todos os campos para criar um post");
+      }
+      const newPost = new Post(idUser, like, comment, description, image);
+      lista.addPost(newPost);
+      res.status(201).json(newPost);
+    } catch (error) {
+      res.status(400).json({ message: error.message, error });
+    }
+  },
+
+  updatePost: (req, res) => {
+    try {
+      res.json(lista.updatePost(req.params.id, req.body));
+    } catch (error) {
+      res.status(404).json({ message: "Erro ao atualizar o post", error });
+    }
+  },
+
+  deletePost: (req, res) => {
+    lista.deletePost(req.params.id);
+    res.status(200).json({ message: "Seu post foi deletado com sucesso", IdDeletado: req.params.id });
+  }
+};
+
+module.exports = router;
